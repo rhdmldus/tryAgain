f3a0a1ff2196cb5f13e8a15ca82b739117ec5b3f
EuiyeonKo <rhdmldus@gmail.com>
2021-05-27 16:36:13 +0900

    removeCharacter event

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -2,14 +2,44 @@ import React, {Component} from 'react'
 import Table from './Table'
 
 class App extends Component {
+    state = {
+        characters: [
+            {
+                name: 'Charlie',
+                job: 'Jnitor',
+            },
+            {
+                name: 'Mac',
+                job: 'Bouncer',
+            },
+            {
+                name: 'Dee',
+                job: 'Aspiring actress',
+            },
+            {
+                name: 'Dennis',
+                job: 'Bartenter'
+            }
+        ],
+    }
     render() {
+        const {characters} = this.state
         return (
-            <div className="App">
-                <h1>Hello, React!</h1>
-                <Table />
+            <div className="container">
+                <Table characterData={characters} removeCharacter={this.removeCharacter} />
             </div>
         )
     }
+    removeCharacter = (index) => {
+        const {characters} = this.state
+    
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index
+            }),
+        })
+    }
 }
+
 
 export default App
